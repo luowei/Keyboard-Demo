17f4d9986a0b4c17e4c0ab3cf082a1ddb0fc46aa
luowei <745054025@qq.com>
2015-01-09 10:39:03 +0800

    first commit ...

---
+++ Constraint-demo/Constraint-Keyboard/KeyboardViewController.h
@@ -0,0 +1,13 @@
+//
+//  KeyboardViewController.h
+//  Constraint-Keyboard
+//
+//  Created by luowei on 15/1/9.
+//  Copyright (c) 2015年 luowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface KeyboardViewController : UIInputViewController
+
+@end
---
+++ Constraint-demo/Constraint-demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Constraint-demo
+//
+//  Created by luowei on 15/1/9.
+//  Copyright (c) 2015年 luowei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
